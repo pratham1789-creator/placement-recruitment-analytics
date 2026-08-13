d51a4ce655ed8ce19d801019b4044fba04b06236
pratham1789-creator <pratham1789@gmail.com>
2026-08-13 20:09:45 +0530

    Add recruitment stage reference data
    Add recruitment stage reference data

---
+++ sql/03_insert_reference_data.sql
@@ -0,0 +1,11 @@
+USE placement_analytics;
+
+INSERT INTO Recruitment_Stages
+(stage_id, stage_name, stage_order)
+VALUES
+(1, 'Applied', 1),
+(2, 'Screening', 2),
+(3, 'Technical Round 1', 3),
+(4, 'Technical Round 2', 4),
+(5, 'Assignment', 5),
+(6, 'Selection', 6);
